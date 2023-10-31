ea02d80e4dee3cee18d9863d052beddb9a2e7afa
Roberson-Andrade <roberson_souza11@hotmail.com>
2023-10-31 11:55:20 -0300

    chore: configure CI/CD

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
 export function App() {
-	return <main className="bg-slate-600">Aquarela</main>;
+	return (
+		<main className="bg-[#F0F9FF] h-screen w-screen">
+			<h1 className="absolute left-[46%] top-[30%] text-3xl">Aquarela</h1>
+		</main>
+	);
 }
